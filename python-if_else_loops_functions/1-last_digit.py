aae938f98c771e9309ccbdc94029b04e0d96fa26
Rashadmlkv <resadmlkv@gmail.com>
2024-01-25 04:34:14 -0800

    string format

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ lastdigit = int(str(number)[-1])
 
 print("Last digit of {} is {}".format(number, lastdigit), end=' ')
 if (lastdigit > 5):
-    print(f"and is greater than 5")
+    print("and is greater than {}".format(5))
 elif (lastdigit < 6 and lastdigit != 0):
-    print(f"and is less than 6 and not 0")
+    print("and is less than {} and not {}".format(6, 0))
 else:
-    print(f"and is 0")
+    print("and is {}".format(0))
