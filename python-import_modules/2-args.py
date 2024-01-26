383c85ad9cb51c76e53033b143ec9c844adfb97b
Rashadmlkv <resadmlkv@gmail.com>
2024-01-26 12:20:29 -0800

    changed to str.format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-
 if (__name__ == "__main__"):
     import sys
     leng = len(sys.argv) - 1
 
     if (leng == 0):
-        print(f"{leng} arguments.")
+        print("{} arguments.".format(leng))
     elif(leng == 1):
-        print(f"{leng} argument:\n{leng}: {sys.argv[1]}")
+        print("{} argument:\n{}: {}".format(leng, leng, sys.argv[1]))
     else:
-        print(f"{leng} arguments:")
-
+        print("{} arguments:".format(leng))
         j = 1
         for i in sys.argv[1:]:
-            print(f"{j}: {i}")
+            print("{}: {}".format(j, i))
             j += 1
