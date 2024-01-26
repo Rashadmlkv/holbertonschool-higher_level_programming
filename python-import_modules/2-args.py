66573e7a8acce18f3de7c23dcfc7102f430aa848
Rashadmlkv <resadmlkv@gmail.com>
2024-01-26 12:28:55 -0800

    changed import method

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if (__name__ == "__main__"):
-    import sys
-    leng = len(sys.argv) - 1
+    from sys import argv
+    leng = len(argv) - 1
 
     if (leng == 0):
         print("{} arguments.".format(leng))
     elif(leng == 1):
-        print("{} argument:\n{}: {}".format(leng, leng, sys.argv[1]))
+        print("{} argument:\n{}: {}".format(leng, leng, argv[1]))
     else:
         print("{} arguments:".format(leng))
         j = 1
-        for i in sys.argv[1:]:
+        for i in argv[1:]:
             print("{}: {}".format(j, i))
             j += 1
