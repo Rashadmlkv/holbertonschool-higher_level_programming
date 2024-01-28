f0c45b5e5713029f6175001379fd55167dc76525
Rashadmlkv <resadmlkv@gmail.com>
2024-01-28 02:58:18 -0800

    fixing

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
+    i = tuple_a + (0, 0)
+    j = tuple_b + (0, 0)
 
-    tuple_c = ()
-    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
-        tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    elif (len(tuple_a) == 1):
-        tuple_c = :wq
-
-
-    return tuple_c
+    return (i[0] + j[0], i[1] + j[1])
