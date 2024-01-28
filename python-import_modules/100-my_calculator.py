a7d48710717417c5c2774f834a3514d0d6038980
Rashadmlkv <resadmlkv@gmail.com>
2024-01-28 04:18:59 -0800

    upload

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+import calculator_1
+
+
+def func():
+    if (len(sys.argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operator = sys.argv[2]
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[3])
+    result = 0
+
+    if (operator == '+'):
+        result = calculator_1.add(num1, num2)
+    elif (operator == '-'):
+        result = calculator_1.sub(num1, num2)
+    elif (operator == '/'):
+        result = calculator_1.div(num1, num2)
+    elif (opeartor == '*'):
+        result = calculator_1.mul(num1, num2)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(num1, operator, num2, result))
+
+
+if (__name__ == "__main__"):
+    func()
