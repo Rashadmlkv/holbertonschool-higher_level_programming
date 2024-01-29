649b41b20075aab89509c4235670a48408a90bd2
Rashadmlkv <resadmlkv@gmail.com>
2024-01-29 10:32:54 -0800

    first

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrixclone = []
+
+
+    print(len(matrix))
+    
+    for i in range(len(matrix)):
+        matrixclone.append([])
+        j = 0
+        while j < len(matrix[i]):
+            matrixclone[i].append(matrix[i][j]**2)
+            j += 1
+    return matrixclone
