f2e1e6eb4cd65925f950eb8470046deaff70b14d
Rashadmlkv <resadmlkv@gmail.com>
2024-01-29 23:08:44 -0800

    added empty list handling

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    newlist = set()
-    newlist = set(my_list)
-    num = list(newlist)[0]
 
-    for i in list(newlist)[1:]:
-        num += i
-    return num
+    if (len(my_list) != 0):
+        newlist = set()
+        newlist = set(my_list)
+        num = list(newlist)[0]
+
+        for i in list(newlist)[1:]:
+            num += i
+        return num
