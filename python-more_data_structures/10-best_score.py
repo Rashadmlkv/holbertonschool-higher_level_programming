2cd07704a3cc7944316bdcc8e8d7102ea241b58d
Rashadmlkv <resadmlkv@gmail.com>
2024-01-30 10:55:25 -0800

    uplaod

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        max = ""
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
+    else:
+        return None
