175b1771911449306a4f45f5e80dc19751bc800f
Rashadmlkv <resadmlkv@gmail.com>
2024-01-30 14:07:52 -0800

    fixed IV and VI inputs' error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,8 +7,21 @@ def roman_to_int(roman_string):
         roman = {"I": 1, "V": 5, "X": 10,
                  "L": 50, "C": 100, "D": 500, "M": 10000}
         sum = 0
+        char = 0
+        while char < len(roman_string):
+            if (char != len(roman_string) - 1 and
+                    roman_string[char] == "I" and
+                    roman_string[char + 1] == "V"):
+                sum += 4
+                char += 2
+            elif (char != len(roman_string) - 1 and
+                    roman_string[char] == "I" and
+                    roman_string[char + 1] == "X"):
+                sum += 9
+                char += 2
+            else:
+                sum += roman[roman_string[char]]
+                char += 1
 
-        for char in roman_string:
-            sum += roman[char]
         return sum
     return 0
