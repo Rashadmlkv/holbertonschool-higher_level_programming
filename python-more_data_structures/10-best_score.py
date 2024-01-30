bd98cf54647413e49e47bd3cab13379392502c64
Rashadmlkv <resadmlkv@gmail.com>
2024-01-30 11:07:38 -0800

    fixed empty dict error

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 def best_score(a_dictionary):
     if a_dictionary is not None:
+        if len(a_dictionary) == 0:
+            return None
         max = ""
         max = list(a_dictionary.keys())[0]
         for key in a_dictionary:
