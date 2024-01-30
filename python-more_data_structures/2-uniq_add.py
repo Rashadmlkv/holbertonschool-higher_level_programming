ddc775d49d7426ad8c0c15b17d632aa472ed7dd9
Rashadmlkv <resadmlkv@gmail.com>
2024-01-29 23:16:27 -0800

    reformatted

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-
-    if (len(my_list) != 0):
-        newlist = set()
-        newlist = set(my_list.copy())
-        num = list(newlist)[0]
-
-        for i in list(newlist)[1:]:
-            num += i
-        return num
+    new_list = set(my_list)
+    return sum(map(lambda x: x, new_list))
