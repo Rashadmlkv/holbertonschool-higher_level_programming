7b6f9edd7e11d766bc3f4746eca42fed32a5de6b
Rashadmlkv <resadmlkv@gmail.com>
2024-02-01 06:34:35 -0800

    changed result keeper

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,13 +2,14 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        i = 0
+        count = 0
 
         for i in range(x):
             print(my_list[i], end='')
+            count += 1
         print()
 
-        return i + 1
+        return count
     except Exception:
         print()
-        return i
+        return count
