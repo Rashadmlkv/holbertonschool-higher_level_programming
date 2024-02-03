f28a2682951e30399af51e27cc127816f373e61f
Rashadmlkv <resadmlkv@gmail.com>
2024-02-03 05:26:45 -0800

    upload

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,7 +14,7 @@ class Square:
 
     def __init__(self, size=0):
         try:
-            if is float():
+            if size is float():
                 raise TypeError
             if size < 0:
                 raise ValueError("size must be >= 0")
