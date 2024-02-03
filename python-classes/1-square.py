e741d50e28e2b904afb28ac48e4002a5b7db40ac
Rashadmlkv <resadmlkv@gmail.com>
2024-02-03 04:15:29 -0800

    upload

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Square class
+"""
+
+
+class Square:
+    """
+    Square class
+
+    Atributes:
+    size: private instance attribute
+    """
+
+    def __init__(self, size):
+        self.__size = size
+    pass
