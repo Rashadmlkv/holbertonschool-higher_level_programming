730b4b5d2a05665596c5577d9fcced8ae3df9a35
Rashadmlkv <resadmlkv@gmail.com>
2024-02-04 07:43:03 -0800

    upload

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    The 0. Integers addition Module
+
+    This module contains add_integer function
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Simple Algorithm that adds two integer and return sum
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a+b
