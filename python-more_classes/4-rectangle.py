ca5483d916259779de3ece0cef234179e3c82724
Rashadmlkv <resadmlkv@gmail.com>
2024-02-08 07:59:27 +0000

    sa

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+    salam
+"""
+
+
+class Rectangle:
+    """
+        necesen
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if (self.width == 0 or self.height == 0):
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        self.array = ""
+        if (self.width == 0 or self.height == 0):
+            return self.array
+        for i in range(self.height):
+            self.array += ("#"*self.width)
+            if i != self.height - 1:
+                self.array += "\n"
+        return self.array
+
+    def __repr__(self):
+        self.array = ""
+        if (self.width == 0 or self.height == 0):
+            return self.array
+        for i in range(self.height):
+            self.array += ("#"*self.width)
+            if i != self.height - 1:
+                self.array += "\n"
+        return self.array
