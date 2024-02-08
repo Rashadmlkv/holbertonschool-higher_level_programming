db3c6850238969b1922268c9d6a1554f069c1c6c
Rashadmlkv <resadmlkv@gmail.com>
2024-02-08 06:50:47 +0000

    asa

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,4 +42,6 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        return 2 * (self.width + self.height )
+        if (self.width == 0 or self.height == 0):
+            return 0
+        return 2 * (self.width + self.height)
