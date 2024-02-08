0233d56276c530ce967553dabda0842c55006b13
Rashadmlkv <resadmlkv@gmail.com>
2024-02-08 12:32:29 +0000

    sA

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -74,3 +74,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if isinstance(rect_2, Rectange):
             raise TypeError("rect_2 must be an instance of Rectangle")
+        if (rect_1 >= rect_2):
+            return rect_1
+        else:
+            return rect_2
