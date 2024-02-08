7a7ec41172467aa6c08e9f725b59b9c047b15e13
Rashadmlkv <resadmlkv@gmail.com>
2024-02-08 12:37:29 +0000

    sa

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -70,11 +70,8 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if isinstance(rect_1, Rectangle):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if isinstance(rect_2, Rectange):
+        if not isinstance(rect_2, Rectange):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if (rect_1 >= rect_2):
-            return rect_1
-        else:
-            return rect_2
+        return max((rect_1, rect_2), key=lambda x: x.area())
