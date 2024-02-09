dca91fe6b5ab4d67720a140a9a98c44e0c61b32f
Rashadmlkv <resadmlkv@gmail.com>
2024-02-09 20:57:00 +0000

    uploading

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' a class MyList that inherits from list: '''
+class MyList(list):
+    ''' def print_sorted(self):prints sorted list '''
+    def print_sorted(self):
+        copylist = self[:]
+        copylist.sort()
+        print(copylist)
