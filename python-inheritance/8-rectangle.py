e01938d6afe3fa741f8603d49ec6c90a38f66c86
Rashadmlkv <resadmlkv@gmail.com>
2024-02-10 14:46:28 +0000

    upload

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+    function
+"""
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator(self, width, width)
+        BaseGeometry.integer_validator(self, height, height)
+
+        self.__width = width
+        self.__height = height
