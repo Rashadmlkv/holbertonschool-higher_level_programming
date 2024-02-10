2c3aede4530573d79c7ac2e80f17904c8b6592b4
Rashadmlkv <resadmlkv@gmail.com>
2024-02-10 10:33:04 +0000

    uplload

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """
-    exactyly instance of class
+    exactly instance of class
 """
 
 
 def is_same_class(obj, a_class):
-"""
-    true if it is , false otherwise
-"""
+    """true if it is , false otherwise"""
 
     return isinstance(obj, a_class)
