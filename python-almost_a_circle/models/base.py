5a19fc16a18cddf57db9e70f7ca25da3f4e4776c
Rashadmlkv <resadmlkv@gmail.com>
2024-02-15 12:35:22 +0000

    json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """first project of python"""
+import json
 
 
 class Base:
@@ -14,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """haliluya"""
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
