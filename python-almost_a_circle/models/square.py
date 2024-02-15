f28e560bb088225cf382285cef2a60fb5c7f0b3c
Rashadmlkv <resadmlkv@gmail.com>
2024-02-15 09:06:59 +0000

    added square function

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""
+    Square class
+"""
+
+
+class Square(Rectangle):
+    """Square class inheriths from rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
