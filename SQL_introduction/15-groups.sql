723fdbcf72fb232fb99cec57a6004a596904aa87
Rashadmlkv <resadmlkv@gmail.com>
2024-03-18 17:34:03 +0000

    upload

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- ists the number of records with the same score
+-- ists the number of records with the same score
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score
+HAVING COUNT(score);
