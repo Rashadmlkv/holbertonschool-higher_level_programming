e919b43aa8f543b64237681029c0631793351bf9
Rashadmlkv <resadmlkv@gmail.com>
2024-03-18 18:01:10 +0000

    upload

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- convert to UTF8
 -- Convert DB
-ALTER TABLE hbtn_0c_0
-CONVERT TO utf8mb4
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the table to UTF8
