c6da3e367cf7afa019c36c547ccd937707d85ad0
Rashadmlkv <resadmlkv@gmail.com>
2024-03-22 18:57:55 +0000

    upload

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- hkm
+-- h
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- m
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- m
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost';
